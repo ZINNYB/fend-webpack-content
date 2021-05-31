1eb83c0e197a7bfb781596a72a12fac07ce10913
ZINNYB <32669452+ZINNYB@users.noreply.github.com>
2021-05-31 20:25:22 +0100

    chore: Add ESlint loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/client/index.js',
-}
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node-modules/,
+        loader: "babel-loader",
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+    ],
+  },
+};
